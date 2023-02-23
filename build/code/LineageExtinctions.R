f2de0260acdb0f9d2ed1a9ece454907510284a81
EliasHadjAmmar <105372928+EliasHadjAmmar@users.noreply.github.com>
2023-02-23 21:52:18 +0100

    build lineage size and extinctions

--- build/code/LineageExtinctions.R
+++ build/code/LineageExtinctions.R
@@ -1,2 +1,34 @@
 # Input: lineage-year panel of rulers.
 # Output: lineage-level data set of last ruler (max start_reign) with death_year.
+
+library(tidyverse)
+
+setwd("~/GitHub/BA")
+
+Main <- function(){
+  rulers <- ImportRulers()
+  last_rulers <- GetLastRulersDeaths(rulers)
+  last_rulers %>% 
+    write.csv("build/temp/last_rulers.csv")
+}
+
+ImportRulers <- function(){
+  rulers <- read.csv("build/temp/rulers.csv")
+  rulers <- rulers %>% 
+    select(id, family, name, terr_id, birth_year, 
+           death_year, start_reign, end_reign)
+  return(rulers)
+}
+
+GetLastRulersDeaths <- function(rulers){
+  # Slightly different results when you filter by max start_reign (one-year reigns).
+  # Strictly speaking, what we care about is the death of the last family member,
+  # not the death of the last ruler. They should be the same though.
+  last_rulers <- rulers %>% 
+    group_by(terr_id) %>% 
+    mutate(last_death_year = max(death_year)) %>% 
+    filter(death_year == last_death_year)
+  return(last_rulers)
+}
+
+Main()
--- build/code/LineageSize.R
+++ build/code/LineageSize.R
@@ -1,2 +1,34 @@
 # Input: city-year panel of ruling lineage.
 # Output: lineage-year panel of no. of cities held.
+
+library(tidyverse)
+library(haven)
+
+setwd("~/GitHub/BA")
+
+Main <- function(){
+  cities <- ImportCities()
+  terr_sizes <- CountCities(cities)
+  terr_sizes %>% 
+    write.csv("build/temp/terr_sizes.csv")
+}
+
+
+ImportCities <- function(){
+  cities <- read_dta("build/input/cities_families_1300_1918.dta")
+  cities <- cities %>% 
+    select(city_id, year, terr_id, foreign_rule)
+  return(cities)
+}
+
+CountCities <- function(cities){
+  terr_sizes <- cities %>% 
+    group_by(terr_id, year) %>% 
+    summarise(count_cities = n())
+  return(terr_sizes)
+}
+
+Main()
+
+
+
