a9d58cad75d60cd437c8ce4fb890f92640624eeb
EliasHadjAmmar <105372928+EliasHadjAmmar@users.noreply.github.com>
2023-03-01 17:09:54 +0100

    #1 fixes duplicate keys bug

--- build/code/LineageExtinctions.R
+++ build/code/LineageExtinctions.R
@@ -8,7 +8,8 @@ setwd("~/GitHub/BA")
 Main <- function(){
   rulers <- ImportRulers()
   last_rulers <- GetLastRulersDeaths(rulers)
-  last_rulers %>% 
+  extinctions <- RemoveDupes(last_rulers)
+  extinctions %>% 
     write.csv("build/temp/last_rulers.csv")
 }
 
@@ -29,6 +30,20 @@ GetLastRulersDeaths <- function(rulers){
     mutate(last_death_year = max(death_year)) %>% 
     filter(death_year == last_death_year)
   return(last_rulers)
+}
+
+RemoveDupes <- function(last_rulers){
+  # Turns the input into a long dataset at the lineage level.
+  # Drops duplicate terr_ids and adds a dummy for affected lineages.
+  # Needed because some terr_ids have two "last rulers", i.e. the two last family
+  # members died in the same year. If we leave both in, this causes duplication
+  # of lineage-year and city-year observations downstream.
+  extinctions <- last_rulers %>% 
+    group_by(terr_id) %>% 
+    mutate(dupe_rulers = if_else(n() > 1, 1, 0)) %>%
+    ungroup() %>% 
+    distinct(terr_id, .keep_all=TRUE)
+  return(extinctions)
 }
 
 Main()
