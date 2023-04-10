e50569e557015e9177ee747e9a2abc18a63d6238
EliasHadjAmmar <105372928+EliasHadjAmmar@users.noreply.github.com>
2023-04-11 00:09:17 +0200

    #4 rework outcome plotting

--- build/code/Construction.R
+++ build/code/Construction.R
@@ -1,6 +1,5 @@
 # Input: city-year panel of construction from Princes and Townspeople.
 # Output: city-year panel of custom aggregates of construction.
-# Currently not functional.
 
 library(tidyverse)
 
