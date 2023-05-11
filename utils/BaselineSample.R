4cc63e7ca9faccae4f937204c7fc2e1f35f6f1ce
EliasHadjAmmar <105372928+EliasHadjAmmar@users.noreply.github.com>
2023-05-11 18:09:27 +0200

    #9 update baseline regression
    note: the baseline estimates change because the sample size increased because I fixed construction_all.csv, which added 3,230 construction events

--- utils/BaselineSample.R
+++ utils/BaselineSample.R
@@ -1,20 +1,13 @@
 BaselineSample <- function(build){
-  
-  # Drop observations with essential variables missing
-  clean <- build |> 
-    DropNACount("year") |>
-    DropNACount("city_id") |> 
-    DropNACount("construction") |> 
-    DropNACount("treatment")
-  
+
   # Drop cities that were treated more than once from the sample
-  max1treat <- clean |>  
+  max1treat <- build |>  
     group_by(city_id) |> 
     mutate(treatments_total = sum(treatment)) |> 
     filter(treatments_total < 2)
   
-  n_dropped <- nrow(clean)-nrow(max1treat)
-  cities_dropped <- n_distinct(clean$city_id)-n_distinct(max1treat$city_id)
+  n_dropped <- nrow(build)-nrow(max1treat)
+  cities_dropped <- n_distinct(build$city_id)-n_distinct(max1treat$city_id)
   
   print(sprintf("Dropped %d observations with multiple treatments (%d cities)", 
                 n_dropped, cities_dropped))
