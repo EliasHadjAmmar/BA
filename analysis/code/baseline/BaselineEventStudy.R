8703140a234531f86e66c8f010d567bab2a39570
EliasHadjAmmar <105372928+EliasHadjAmmar@users.noreply.github.com>
2023-05-11 19:27:01 +0200

    #10 add aggregation to event study

--- analysis/code/baseline/BaselineEventStudy.R
+++ analysis/code/baseline/BaselineEventStudy.R
@@ -9,20 +9,45 @@ library(fixest)
 setwd("~/GitHub/BA")
 
 source("utils/BaselineSample.R")
+source("utils/AggregateYears.R")
 
 Main <- function(){
-  
-  NLEADS <- 10
-  NLAGS <- 15
-  
+
   build <- read_csv("analysis/input/build.csv", show_col_types = FALSE)
-  clean <- BaselineSample(build)
-  with_window <- AddLeadsLags(clean, NLEADS, NLAGS)
+  bsample <- BaselineSample(build)
   
+  YearlyES(bsample, t_lead=5, t_lag=10)
+  PeriodES(bsample, t_lead=20, t_lag=40, spacing=5)
+  
+}
+
+
+AddLeadsLags <- function(clean, t_lead, t_lag, tname="year"){
+  timing <- clean |> 
+    filter(treatment == 1) |> 
+    mutate(treat_time = !!sym(tname)) |> 
+    select(city_id, treat_time)
+  
+  clean_with_timing <- clean |> 
+    left_join(timing, by="city_id") |> 
+    mutate(treat = ifelse(is.na(treat_time), 0, 1))
+  
+  clean_with_window <- clean_with_timing |> 
+    mutate(time_to_treat = ifelse(treat == 1, !!sym(tname)-treat_time, 0)) |> # create time-to-treat
+    mutate(treat = ifelse(time_to_treat %in% -t_lead:t_lag, 1, 0)) |> # set treat=0 if not in window
+    mutate(time_to_treat = ifelse(treat==0, 0, time_to_treat)) # set redundant times-to-treat vals =0
+  
+  return(clean_with_window)
+}
+
+
+YearlyES <- function(bsample, t_lead, t_lag){
+
+  with_dummies <- AddLeadsLags(bsample, t_lead, t_lag, tname="year")
   
   mod <- fixest::feols(construction ~ i(time_to_treat, treat, ref = -1) |  
-                            city_id + year, data = with_window)
-  
+                            city_id + year, data = with_dummies)
+
   iplot(mod)
   
   
@@ -33,22 +58,16 @@ Main <- function(){
   # write(tex_output, file="analysis/output/tables/baseline_did.tex")
 }
 
-AddLeadsLags <- function(clean, nleads, nlags){
-  timing <- clean |> 
-    filter(treatment == 1) |> 
-    mutate(treat_year = year) |> 
-    select(city_id, treat_year)
+
+PeriodES <- function(bsample, t_lead, t_lag, spacing){
   
-  clean_with_timing <- clean |> 
-    left_join(timing, by="city_id") |> 
-    mutate(treat = ifelse(is.na(treat_year), 0, 1))
+  agg_sample <- AggregateYears(bsample, spacing)
+  with_dummies <- AddLeadsLags(agg_sample, t_lead, t_lag, tname="period")
   
-  clean_with_window <- clean_with_timing |> 
-    mutate(time_to_treat = ifelse(treat == 1, year-treat_year, 0)) |> # create time-to-treat
-    mutate(treat = ifelse(time_to_treat %in% -nleads:nlags, 1, 0)) |> # set treat=0 if not in window
-    mutate(time_to_treat = ifelse(treat==0, 0, time_to_treat)) # set redundant times-to-treat vals =0
+  mod <- fixest::feols(construction ~ i(time_to_treat, treat, ref = -spacing) |  
+                         city_id + period, data = with_dummies)
   
-  return(clean_with_window)
+  iplot(mod)
 }
 
 
