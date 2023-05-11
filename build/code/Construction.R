e6f766a54ea57ebfecbf42c5e4b8b77e96a84c4d
EliasHadjAmmar <105372928+EliasHadjAmmar@users.noreply.github.com>
2023-05-11 17:33:41 +0200

    #9 fix build (construction part)

--- build/code/Construction.R
+++ build/code/Construction.R
@@ -6,19 +6,26 @@ suppressPackageStartupMessages(library(tidyverse))
 setwd("~/GitHub/BA")
 
 Main <- function(){
-  cons_raw <- read_csv("build/input/construction_all.csv", show_col_types = F) |> 
+  
+  # Note: the construction_all.csv file I got from Prof. Cantoni couldn't be read properly,
+  # with values being parsed into the wrong column for 3,230 rows. I was able to fix this
+  # by reading the file into an Excel sheet and exporting it back to .csv.
+  cons_raw <- read_delim("build/input/construction_all_xl.csv", delim = ";", show_col_types = F) |> 
     rename(year = time_point)
+  
   cons <- CountBuildings(cons_raw)
   cons_gaps <- AddGaps(cons)
+  
   cons_clean <- TidyOutput(cons_gaps)
   cons_clean |> write_csv("build/temp/construction_new.csv")
+  
 }
 
 
 CountBuildings <- function(cons_raw, exclude=NULL){
   # experiment with filter settings!
   cons <- cons_raw |> 
-    filter(!building %in% exclude & uncertainty==0) |> 
+    filter(!building %in% exclude & uncertainty==0) |> # only precise years
     group_by(city_id, year) |> 
     summarise(construction = n())
   return(cons)
@@ -28,19 +35,19 @@ CountBuildings <- function(cons_raw, exclude=NULL){
 AddGaps <- function(cons){
   all_keys <- cons |> 
     group_by(city_id) |> 
-    expand(full_seq(year, 1)) |> # change to 1 later
+    expand(full_seq(year, 1)) |>
     rename(year = `full_seq(year, 1)`)
   
-   with_gaps <- cons |> 
-     right_join(all_keys, by=c("city_id", "year")) |> 
+   with_gaps <- all_keys |> 
+     left_join(cons, by=c("city_id", "year")) |> 
      replace_na(list(construction = 0)) |> 
      arrange(city_id, year)
    
    return(with_gaps)
 }
 
+
 TidyOutput <- function(cons){
-  # change this when you get the full data
   clean <- cons |> 
     select(city_id, year, construction) |> 
     mutate(construction_any = as.numeric(construction > 0))
@@ -48,8 +55,5 @@ TidyOutput <- function(cons){
   return(clean)
 }
 
-# There are some impurities in the new data - specifically, some entries have comments
-# as city ids and single-digit years. But they should fall away with the join in
-# CombineCityData.R, I think.
 
-test <- Main()
+Main()
