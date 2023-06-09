826db4562e1d158e324db9ff64de80f44afa37ff
EliasHadjAmmar <105372928+EliasHadjAmmar@users.noreply.github.com>
2023-06-10 01:05:27 +0200

    #23 get all switches directly

---
+++ newbuild/CitiesSwitches.R
@@ -0,0 +1,29 @@
+# Input: yearly territorial histories of cities.
+# Output: same, but with dummies for switching years, conquests, and successions.
+
+suppressPackageStartupMessages(library(tidyverse))
+suppressPackageStartupMessages(library(haven))
+
+setwd("~/GitHub/BA")
+
+Main <- function(){
+  cities <- read_dta("build/input/cities_families_1300_1918.dta")
+  cities <- cities |> 
+    select(city_id, year, terr_id, type_change)
+
+  CONQUEST_CATS <- c(4, 7) # "Conquest", "Acquisition by conflict"
+  SUCCESSION_CATS <- c(3, 11) # "Extinction of lineage", "Inheritance", NOT "Marriage"
+  
+  cities_with_switch_dummies <- cities |> 
+    group_by(city_id, terr_id) |> 
+    mutate(switch = if_else(year == min(year), 1, 0)) |> 
+    mutate(
+      conquest = if_else(type_change %in% CONQUEST_CATS, 1, 0),
+      succession = if_else(type_change %in% SUCCESSION_CATS, 1, 0)
+      ) |> 
+    select(-type_change)
+  
+  write_csv(cities_with_switch_dummies, "newbuild/temp/cities_switches.csv")
+}
+
+Main()
