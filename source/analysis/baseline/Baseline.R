da761d90983d8443c21b6cbb24fd62d7128b9899
EliasHadjAmmar <105372928+EliasHadjAmmar@users.noreply.github.com>
2023-06-21 12:16:23 +0200

    #25 and different outcomes and improve baseline table

--- source/analysis/baseline/Baseline.R
+++ source/analysis/baseline/Baseline.R
@@ -23,20 +23,44 @@ Main <- function(){
                      binarise_switches = T)
   
   # Estimate my baseline interaction equation
-  mod_basic <- fixest::feols(
-    c_all ~ i(rule_conquest, D, ref=0) + i(rule_other, D, ref=0) + D + switches | city_id + period, 
+  mod_all_basic <- fixest::feols(
+    c_all ~ D + i(rule_conquest, D, ref=0) + i(rule_other, D, ref=0) + switches | city_id + period, 
     data = dat)
   
-  mod_conflict <- fixest::feols(
-    c_all ~ i(rule_conquest, D, ref=0) + i(rule_other, D, ref=0) + D + switches + conflict | city_id + period, 
+  mod_state_basic <- fixest::feols(
+    c_state ~ D + i(rule_conquest, D, ref=0) + i(rule_other, D, ref=0) + switches | city_id + period, 
     data = dat)
+  
+  mod_priv_basic <- fixest::feols(
+    c_private ~ D + i(rule_conquest, D, ref=0) + i(rule_other, D, ref=0) + switches | city_id + period, 
+    data = dat)
+  
+  mod_pub_basic <- fixest::feols(
+    c_public ~ D + i(rule_conquest, D, ref=0) + i(rule_other, D, ref=0) + switches | city_id + period, 
+    data = dat)
+  
+  # mod_all_conflict <- fixest::feols(
+  #   c_all ~ i(rule_conquest, D, ref=0) + i(rule_other, D, ref=0) + D + switches + conflict | city_id + period, 
+  #   data = dat)
+  
   
   # Produce regression table and export to LaTeX
   etableDefaults()
-  tex_output <- etable(mod_basic, mod_conflict, tex=TRUE,
-                       title="Test Title",
+  
+  note <- paste("Table presents results of estimation equation
+  \\eqref{eq:baseline}.", PeriodInsert(1), "Observations are at the city-period 
+  level. The dependent variables are indicators that take the value 1 if 
+  construction activity of the respective type was recorded. Standard errors are 
+  clustered at the city level. The switch type \"Succession\" is omitted as the 
+  reference category.", sep = " ") |> str_replace_all("\n ", "")
+  
+  tex_output <- etable(mod_all_basic, mod_state_basic,
+                       mod_priv_basic, mod_pub_basic,
+                       tex=TRUE,
+                       title="Differences in trends after switching, by switch type",
                        label = sprintf("tab:baseline_%iy", t),
-                       postprocess.tex = PostProcessBaseline)
+                       postprocess.tex = PostProcessBaseline,
+                       notes = note)
   
   filename <- sprintf("paper/output/regressions/baseline_%iy.tex", t)
   write(tex_output, file=filename)
--- source/utils/etableDefaults.R
+++ source/utils/etableDefaults.R
@@ -2,15 +2,25 @@ etableDefaults <- function(){
   
   # uniform defaults (across all tables)
   setFixest_etable(
-    se.below = FALSE,
+    se.below = TRUE,
     digits = 2,
     digits.stats = 4,
   )
   
   # set default dict
   setFixest_dict(c(city_id = "City", period = "Period",
-                   c_all = "Construction", D = "Switch to another state",
+                   c_all = "All construction", 
+                   c_state = "State construction",
+                   c_private = "Private construction",
+                   c_public = "Public goods",
+                   D = "Switch to another state",
                    rule_conquest = "Conquest", rule_succession = "Succession",
                    rule_other = "Other", switches = "Switching indicator",
                    conflict = "Conflict"))
+}
+
+PeriodInsert <- function(t){
+  period_insert <- sprintf("Yearly data was aggregated into periods of %i years.", t)
+  if (t == 1) { period_insert <- "Time periods have a length of 1 year." }
+  return(period_insert)
 }
