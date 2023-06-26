515b70c52a7899999c7e2827f064058bed02e046
EliasHadjAmmar <105372928+EliasHadjAmmar@users.noreply.github.com>
2023-06-26 13:17:57 +0200

    #29 fix typo in header

--- source/analysis/robustness/Robustness.R
+++ source/analysis/robustness/Robustness.R
@@ -69,7 +69,7 @@ Main <- function(){
   clustered at the city level.", SignifInsert(), sep = " ") |> 
     str_replace_all("\n ", "")
   
-  header <- c("Baseline", "4 Switches", "All switches", "Window")
+  header <- c("Baseline", "4 switches", "All switches", "Window")
   
   tex_output <- etable(mod_baseline, mod_4switches, 
                        mod_allswitches, mod_window,
