6175c772cfb714e7a8241a479a8cda94649bee1e
EliasHadjAmmar <105372928+EliasHadjAmmar@users.noreply.github.com>
2023-06-26 22:15:40 +0200

    #29 include check in controls table

--- source/analysis/robustness/Controls.R
+++ source/analysis/robustness/Controls.R
@@ -7,6 +7,7 @@ setwd("~/GitHub/BA")
 
 source("source/utils/HandleCommandArgs.R")
 source("source/utils/DataPrepSuite.R")
+source("source/utils/PrepareBaselineData.R")
 source("source/utils/etableDefaults.R")
 
 Main <- function(){
@@ -17,10 +18,13 @@ Main <- function(){
   build <- read_csv(read_path, show_col_types = F)
   
   # Select sample and process data (exact steps documented below)
-  dat <- PrepareData(build,
+  dat <- PrepareBaselineData(build,
                      max_switches = 2,
                      binarise_construction = T,
                      binarise_switches = T)
+  
+  dat_noconflict <- dat |> 
+    drop_na(conflict)
   
   # Estimate Equation (2) with different sets of controls
   mod_baseline <- fixest::feols(
@@ -39,13 +43,16 @@ Main <- function(){
     c_all ~ D + i(rule_conquest, D, ref=0) + i(rule_other, D, ref=0) + conflict | city_id + period, 
     data = dat)
   
+  mod_conf_check <- fixest::feols(
+    c_all ~ D + i(rule_conquest, D, ref=0) + i(rule_other, D, ref=0) + switches | city_id + period, 
+    data = dat_noconflict)
   
   # Produce regression table and export to LaTeX
   etableDefaults()
   
   note <- paste("Note: Table presents results of estimation equation
-  \\eqref{eq:baseline} but using different control variables. The switch type 
-  \"Succession\" is omitted as the reference category.", PeriodInsert(t), 
+  \\eqref{eq:baseline} but using different control variables. Columns (3)-(5) drop
+  observations with missing data on conflict incidents from the sample.", PeriodInsert(t), 
   "Observations are at the city-period level. The dependent variable is an 
   indicator that takes the value 1 if any construction activity was recorded. 
   Standard errors are clustered at the city level.", SignifInsert(), sep = " ") |> 
@@ -53,6 +60,7 @@ Main <- function(){
   
   tex_output <- etable(mod_baseline, mod_noswitches,
                        mod_conflict, mod_conf_nosw,
+                       mod_conf_check,
                        tex=TRUE,
                        title="Heterogeneous effects of switching: Robustness",
                        label = sprintf("tab:controls_%iy", t),
@@ -67,38 +75,6 @@ Main <- function(){
 }
 
 
-PrepareData <- function(build, max_switches,
-                        binarise_construction, binarise_switches){
-  
-  # Filter out cities with too many lifetime switches
-  selected <- FilterBySwitches(build, max_switches)
-  
-  # Identify across-period switches
-  with_e_dummies <- AddEAnother(selected)
-  
-  # Add TREAT x POST dummies D
-  with_D <- AddTreatXPost(with_e_dummies)
-  
-  # Binarise construction outcomes, if specified
-  if (binarise_construction) { with_D <- with_D |> BinariseOutcomes() }
-  
-  # Binarise no. of switches (control), if specified
-  if (binarise_switches) { with_D <- with_D |> BinariseSwitches() }
-  
-  # Process type change into treatment type
-  with_types <- ProcessTypeChange(with_D)
-  
-  # Rearrange columns (for readability)
-  clean <- with_types |> select(
-    city_id, treat_time, period, terr_id, switches, e_another, D, 
-    rule_conquest, rule_succession, rule_other,
-    conflict, c_all, c_state, c_private, c_public
-  )
-  
-  return(clean)
-}
-
-
 PostProcessBaseline <- function(tex_output){
   # Post-processing the TeX string to make it neater
   # If the number of columns changes, the last two lines may show up again
@@ -106,8 +82,8 @@ PostProcessBaseline <- function(tex_output){
   tex_post <- tex_output |> 
     str_replace("Conquest \\$=\\$ 1", "Conquest") |> 
     str_replace("Other \\$=\\$ 1", "Other") |> 
-    str_replace(fixed("\\multicolumn{5}{l}{\\emph{Clustered (City) standard-errors in parentheses}}\\\\"), "") |> 
-    str_replace(fixed("\\multicolumn{5}{l}{\\emph{Signif. Codes: ***: 0.01, **: 0.05, *: 0.1}}\\\\"), "")
+    str_replace(fixed("\\multicolumn{6}{l}{\\emph{Clustered (City) standard-errors in parentheses}}\\\\"), "") |> 
+    str_replace(fixed("\\multicolumn{6}{l}{\\emph{Signif. Codes: ***: 0.01, **: 0.05, *: 0.1}}\\\\"), "")
   return(tex_post)
 }
 
