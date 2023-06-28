c118b9d21ee86e8361bb5796efa61d67e2e37b69
EliasHadjAmmar <105372928+EliasHadjAmmar@users.noreply.github.com>
2023-06-29 01:51:05 +0200

    #33 add terr maps

--- source/slides/MapTerrs.R
+++ source/slides/MapTerrs.R
@@ -16,92 +16,30 @@ Main <- function(){
     select(city_id, region_id, name, nat)
   locs_spatial <- st_read("drive/raw/attributes/city_borders")
   
+  # Create map
+  CleanTerrsMap(1700, terrs, locs_spatial)
   
-  BasicTerrsMapOneYear(1700, terrs, locs_spatial)
+  # Create and save maps at different years
+  time_points <- c(1300, 1500, 1800, 1917)
   
-  # Get snapshot of city ownership in 1500
-  dat1500_sf <- GetGeodataOneYear(1500, terrs, locs_spatial)
-  
-  
-  # Make a spatial dataset of region polygons
-  terrs1500 <- unique(dat1500_sf$terr_id) |> sort() |> `names<-`("terr_id")
-  
-  polygons <- terrs1500 |> 
-    purrr::map(\(id)(TerrIDtoUnion(id, dat1500_sf))) |> 
-    bind_rows()
-  
-  # Bind the cities to region polygons (how about a spatial join?)
-  terrs1500_sf <- bind_cols(terrs1500, polygons) |> 
-    rename(terr_id = ...1) |> 
-    st_as_sf()
-  
-  ### Create heatmap of city counts
-  # Increase the bounding box of the spatial data to leave space for title
-  bbox_new <- BiggerBoundingBox(terrs1500_sf, scale_top = 0.1)
-  
-  # Create basic map
-  map1 <- tm_shape(terrs1500_sf, 
-                   bbox = bbox_new)+
-    tm_polygons(col="terr_id", 
-                title="No. of cities") +
-    tm_layout(title = "Spatial distribution of cities - Full set",
-              title.size = 1.3,
-              title.position = c("center", "top"),
-              title.fontface = 2,
-              legend.title.size = 1.3,
-              legend.text.size = 1)
-  
-  map1
-  
-  # Add an unlabelled dot for each city in the build
-  allpoints_sf <- locs_spatial |> 
-    filter(city_id %in% unique(build$city_id))
-  
-  map2 <- map1 + 
-    tm_shape(allpoints_sf)+
-    tm_dots(alpha = 0.3)
-  
-  
-  # Add some city names as reference points
-  citynames <- c("Muenchen", "Berlin", "Koenigsberg Pr.", "Koeln", 
-                 "Hannover", "Breslau", "Wuerzburg", "Dresden",
-                 "Kiel", "Stettin", "Kassel")
-  
-  points_sf <- locs_spatial |> 
-    filter(name %in% citynames) |> 
-    CleanNames()
-  
-  map3 <- map2 +
-    tm_shape(points_sf)+
-    tm_dots()+
-    tm_text("name", size=1, 
-            just="bottom", 
-            ymod=0.35,
-            fontface = 2)
-  
-  # Save map as PNG
-  filename <- "paper/output/descriptive/map_cities_raw.png"
-  tmap_save(map3, filename)
+  for (t in time_points) {
+    map1 <- CleanTerrsMap(t, terrs, locs_spatial)
+    filename <- sprintf("paper/output/slides/map_terrs_%i.png", t)
+    tmap_save(map1, filename)
+  }
   
   return(0)
 }
 
-BasicTerrsMapOneYear <- function(t, terrs, locs_spatial){
+
+BasicTerrsMap <- function(t, terrs, locs_spatial){
   
-  # Get snapshot of city ownership in year t
-  dat_t_sf <- GetGeodataOneYear(t, terrs, locs_spatial)
+  terrs_oneyear <- GetGeodataOneYear(t, terrs, locs_spatial)
+  terrs_t_sf <- GetPolygonData(terrs_oneyear)
   
-  # Make a spatial dataset of region polygons
-  terrs_t <- unique(dat_t_sf$terr_id) |> sort() |> `names<-`("terr_id")
-  
-  polygons <- terrs_t |> 
-    purrr::map(\(id)(TerrIDtoUnion(id, dat_t_sf))) |> 
-    bind_rows()
-  
-  # Bind the cities to region polygons (how about a spatial join?)
-  terrs_t_sf <- bind_cols(terrs_t, polygons) |> 
-    rename(terr_id = ...1) |> 
-    st_as_sf()
+  # INSERT SINGLETON HANDLING STEPS HERE
+  # 
+  # 
   
   ### Create heatmap of city counts
   # Increase the bounding box of the spatial data to leave space for title
@@ -111,17 +49,42 @@ BasicTerrsMapOneYear <- function(t, terrs, locs_spatial){
   map1 <- tm_shape(terrs_t_sf, 
                    bbox = bbox_new)+
     tm_polygons(col="terr_id", 
-                title="Territory")
+                title="Territory")+
+    tm_layout(title = sprintf("Territories in %i", t),
+              title.size = 1.3,
+              title.position = c("center", "top"),
+              title.fontface = 2,
+              legend.show = F)
   return(map1)
 }
 
 
+CleanTerrsMap <- function(t, terrs, locs_spatial){
+  
+  # Create basic map
+  map1 <- BasicTerrsMap(t, terrs, locs_spatial)
+  
+  # Add some city names as reference points (if they exist at t)
+  citynames <- c("Muenchen", "Berlin", "Koenigsberg Pr.", "Koeln", 
+                 "Hannover", "Breslau", "Wuerzburg", "Dresden",
+                 "Kiel", "Stettin", "Kassel")
+  
+  points_sf <- GetGeodataOneYear(t, terrs, locs_spatial) |> 
+    filter(name %in% citynames) |> 
+    CleanNames()
+  
+  map2 <- map1 +
+    tm_shape(points_sf)+
+    tm_dots()+
+    tm_text("name", size=1, 
+            just="bottom", 
+            ymod=0.35,
+            fontface = 2)
+  
+  return(map2)
+}
 
 
-dat1500_sf <- GetGeodataOneYear(1500, terrs, locs_spatial)
-BasicTerrsMapOneYear(1300, terrs, locs_spatial)
-BasicTerrsMapOneYear(1600, terrs, locs_spatial)
-BasicTerrsMapOneYear(1800, terrs, locs_spatial)
-BasicTerrsMapOneYear(1917, terrs, locs_spatial)
+
 
 Main()
--- source/slides/TerrMapUtils.R
+++ source/slides/TerrMapUtils.R
@@ -6,11 +6,34 @@ TerrIDtoUnion <- function(id, locs_spatial){
 }
 
 
+GetPolygonData<- function(terrs_oneyear){
+  
+  # Make a spatial dataset of region polygons
+  terrs_t <- unique(terrs_oneyear$terr_id) |> sort() |> `names<-`("terr_id")
+  
+  polygons <- terrs_t |> 
+    purrr::map(\(id)(TerrIDtoUnion(id, terrs_oneyear))) |> 
+    bind_rows()
+  
+  # Bind the cities to region polygons
+  terrs_t_sf <- bind_cols(terrs_t, polygons) |> 
+    rename(terr_id = ...1)
+  
+  # Add info on city counts (to later recode singleton terrs)
+  with_city_counts <- terrs_oneyear |> 
+    group_by(terr_id) |> 
+    summarise(n_cities = n_distinct(city_id)) |> 
+    left_join(terrs_t_sf, by="terr_id") |> 
+    st_as_sf()
+  
+  return(with_city_counts)
+}
+
+
 GetGeodataOneYear <- function(t, terrs, locs_spatial){
   terrs_oneyear <- terrs |> 
     filter(year == t) |> 
-    mutate(terr_id = if_else(city_status != 0, "00_FREE", terr_id)) |> 
-    select(city_id, terr_id, city_status) |> 
+    select(city_id, terr_id) |> 
     left_join(locs_spatial, by="city_id") |> 
     st_as_sf()
   
